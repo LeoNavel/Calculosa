2889b073980b667fd916d0524f32561d2d35d6e9
Michal Bohus <zololol3@gmail.com>
2016-04-01 20:15:40 +0200

    Created basics of mathlib class and its methods

---
+++ app/js/Mathlib/Mathlib.js
@@ -0,0 +1,31 @@
+/**Math library for our calculator**/
+function Mathlib() {
+
+    this.addition = function(a,b){
+
+    };
+
+    this.subtitution = function(a,b){
+
+    };
+
+    this.multiplication = function(a,b){
+
+    };
+
+    this.logarithm = function(a){
+
+    };
+
+    this.power = function(a){
+
+    };
+
+    this.division = function(a,b){
+
+    };
+
+    this.factorial = function(a){
+
+    };
+}
