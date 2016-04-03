37338a021773ce4c475518cf74910048f779fe1b
Michal Bohus <zololol3@gmail.com>
2016-04-03 11:06:05 +0200

    Added doxygen style comments

--- app/js/Mathlib/Mathlib.js
+++ app/js/Mathlib/Mathlib.js
@@ -1,30 +1,84 @@
 /**Math library for our calculator**/
 function Mathlib() {
 
+    /** @brief Addition
+     *
+     *  Adds b to a
+     *
+     *	@param [in] a number
+     *	@param [in] b number
+     *	@return a
+     */
     this.addition = function(a,b){
 
     };
 
+    /** @brief Subtitution
+     *
+     *  Subtitute b from a
+     *
+     *	@param [in] a number
+     *	@param [in] b number
+     *	@return a
+     */
     this.subtitution = function(a,b){
 
     };
 
+    /** @brief Multiplication
+     *
+     *  Multiply a and b
+     *
+     *	@param [in] a number
+     *	@param [in] b number
+     *	@return a
+     */
     this.multiplication = function(a,b){
 
     };
 
+    /** @brief Logarithm
+     *
+     *  Calculate logarithm from a
+     *
+     *	@param [in] a number
+     *	@return a
+     */
     this.logarithm = function(a){
 
     };
 
-    this.power = function(a){
+    /** @brief Power
+     *
+     *  Empower a to b
+     *
+     *	@param [in] a number
+     *	@param [in] b number
+     *	@return a
+     */
+    this.power = function(a,b){
 
     };
 
+    /** @brief Division
+     *
+     *  A divided by b
+     *
+     *	@param [in] a number
+     *	@param [in] b number
+     *	@return a
+     */
     this.division = function(a,b){
 
     };
 
+    /** @brief Factorial
+     *
+     *  Calculate factorial
+     *
+     *	@param [in] a number
+     *	@return a
+     */
     this.factorial = function(a){
 
     };
