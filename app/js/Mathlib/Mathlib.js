55406314101bb0d6ccebe86582b3f04f50cca165
Michal Bohus <zololol3@gmail.com>
2016-04-08 21:37:37 +0200

    After code review by Filip, made several changes

--- app/js/Mathlib/Mathlib.js
+++ app/js/Mathlib/Mathlib.js
@@ -1,85 +1,83 @@
 /**Math library for our calculator**/
-function Mathlib() {
 
-    /** @brief Addition
-     *
-     *  Adds b to a
-     *
-     *	@param [in] a number
-     *	@param [in] b number
-     *	@return a
-     */
-    this.addition = function(a,b){
+/** @brief Addition
+ *
+ *  Adds b to a
+ *
+ *	@param [in] a number
+ *	@param [in] b number
+ *	@return a
+ */
+exports.addition = function(a,b){
 
-    };
+};
 
-    /** @brief Subtitution
-     *
-     *  Subtitute b from a
-     *
-     *	@param [in] a number
-     *	@param [in] b number
-     *	@return a
-     */
-    this.subtitution = function(a,b){
+/** @brief Subtraction
+ *
+ *  Subtracts b from a
+ *
+ *	@param [in] a number
+ *	@param [in] b number
+ *	@return a
+ */
+exports.subtraction = function(a,b){
 
-    };
+};
 
-    /** @brief Multiplication
-     *
-     *  Multiply a and b
-     *
-     *	@param [in] a number
-     *	@param [in] b number
-     *	@return a
-     */
-    this.multiplication = function(a,b){
+/** @brief Multiplication
+ *
+ *  Multiply a and b
+ *
+ *	@param [in] a number
+ *	@param [in] b number
+ *	@return a
+ */
+exports.multiplication = function(a,b){
 
-    };
+};
 
-    /** @brief Logarithm
-     *
-     *  Calculate logarithm from a
-     *
-     *	@param [in] a number
-     *	@return a
-     */
-    this.logarithm = function(a){
+/** @brief Logarithm
+ *
+ *  Calculate logarithm from a
+ *
+ *	@param [in] a number
+ *	@return a
+ */
+exports.logarithm = function(a){
 
-    };
+};
 
-    /** @brief Power
-     *
-     *  Empower a to b
-     *
-     *	@param [in] a number
-     *	@param [in] b number
-     *	@return a
-     */
-    this.power = function(a,b){
+/** @brief Power
+ *
+ *  Empower a to b
+ *
+ *	@param [in] a number
+ *	@param [in] b number
+ *	@return a
+ */
+exports.power = function(a,b){
 
-    };
+};
 
-    /** @brief Division
-     *
-     *  A divided by b
-     *
-     *	@param [in] a number
-     *	@param [in] b number
-     *	@return a
-     */
-    this.division = function(a,b){
+/** @brief Division
+ *
+ *  A divided by b
+ *
+ *	@param [in] a number
+ *	@param [in] b number
+ *	@return a
+ */
+exports.division = function(a,b){
 
-    };
+};
 
-    /** @brief Factorial
-     *
-     *  Calculate factorial
-     *
-     *	@param [in] a number
-     *	@return a
-     */
-    this.factorial = function(a){
+/** @brief Factorial
+ *
+ *  Calculate factorial
+ *
+ *	@param [in] a number
+ *	@return a
+ */
+exports.factorial = function(a){
 
-    };
-}
+};
