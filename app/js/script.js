17da17315bb938a2fb7016ad34404a2dbb48c290
Filip Klembara <leo@navel.pro>
2016-04-09 19:12:52 +0200

    just some refactoring to improve work with GUI in future and now you can change style with click on change style

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,26 @@
-$(function () {
+var style=0;
+$(function(){
     $("#output").append("Hello Calculosa from jQuery!");
+    $(".number").click(function(){
+        alert($(this).text() + " clicked");
+    });
+    $("#changeStyle").click(function(){
+        changeStyle();
+    });
 });
+
+function changeStyle(){
+    if(style==1){
+        $(".btn").css("background-color", "#B2E5F0");
+        $("#output").css("background-color", "#B2E5F0");
+        $("img").css("color", "#B2E5F0");
+        $("body").css("background-image", "url(../images/background.png");
+        style=0;
+    }else{
+        $(".btn").css("background-color", "");
+        $("#output").css("background-color", "");
+        $("img").css("color", "");
+        $("body").css("background-image", "");
+        style=1;
+    }
+}
