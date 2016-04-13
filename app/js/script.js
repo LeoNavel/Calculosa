60e94c433a2f294faa899509eb9300ea224c2e01
Filip Klembara <leo@navel.pro>
2016-04-14 00:45:17 +0200

    some code reformat and add sign change

--- app/js/script.js
+++ app/js/script.js
@@ -5,41 +5,66 @@ var Calculosa = new calculosa;
 var olejvan = {state: 1, vars: {a: "", b: "", operation: ""}};
 $(function(){
     $(".number").click(function(){
-        if(olejvan.state==3){
-            //todo
-        }
-        var outputElement = $("#output");
-        var str = outputElement.text();
-        if(str.length<30){
-            str += $(this).text();
-            outputElement.text(str);
-        }else{
-            Calculosa.err("screen overflow");  // todo calculosa error
-        }
+        addNumber($(this));
     });
 
     $(".point").click(function(){
-        var outputElement = $("#output");
-        var str = outputElement.text();
-        if(str.length>0){
-            if(str.slice(-1)==".")
-                str = str.slice(0, -1);
-            else if(str.indexOf(".")<0)
-                str += ".";
-            else{
-                Calculosa.err("you can have just one point");  //todo calculosa error
-                return;
-            }
-            outputElement.text(str);
-        }else
-            outputElement.text("0.");
+        floatingPoint();
     });
 
+    $(".sign").click(function(){
+        changeSign();
+    });
 
     $("#changeStyle").click(function(){
         changeStyle();
     });
 });
+
+function changeSign(){
+    var outputElement = $("#output");
+    var str = outputElement.text();
+    if(str.length>0){
+        if(str.substr(0, 1)=="-")
+            str = str.substr(1);
+        else
+            str = "-" + str;
+        outputElement.text(str);
+    }else{
+        Calculosa.err("firstly enter a number"); //todo calculosa error
+    }
+}
+
+function addNumber(element){
+    if(olejvan.state==3){
+        //todo
+    }
+    var outputElement = $("#output");
+    var str = outputElement.text();
+    if(str.length<30){
+        str += element.text();
+        outputElement.text(str);
+    }else{
+        Calculosa.err("screen overflow");  // todo calculosa error
+    }
+}
+
+function floatingPoint(){
+    var outputElement = $("#output");
+    var str = outputElement.text();
+    if(str.length>0){
+        if(str.slice(-1)==".")
+            str = str.slice(0, -1);
+        else if(str.indexOf(".")<0)
+            str += ".";
+        else{
+            Calculosa.err("you can have just one point");  //todo calculosa error
+            return;
+        }
+        outputElement.text(str);
+    }else
+        outputElement.text("0.");
+}
 
 function changeStyle(){
     if(style==1){
