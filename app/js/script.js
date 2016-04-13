1dd3480773156fc57cd9580f8b42b6c5cda78ec1
Filip Klembara <leo@navel.pro>
2016-04-14 00:10:48 +0200

    now can read numbers from buttons

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,23 @@
-var style=0;
+/**Math library for our calculator**/
+var style = 0;
+var Mathlib = require('../js/Mathlib/Mathlib.js');
+var olejvan = {state: 1, vars: {a: "", b: "", operation: ""}};
 $(function(){
-    $("#output").append("Hello Calculosa from jQuery!");
     $(".number").click(function(){
-        alert($(this).text() + " clicked");
+        if(olejvan.state==3){
+            //todo
+        }
+        var outputElement = $("#output");
+        var str = outputElement.text();
+        if(str.length<30){
+            str += $(this).text();
+            outputElement.text(str);
+        }else{
+            // todo calculosa error
+        }
     });
+
+
     $("#changeStyle").click(function(){
         changeStyle();
     });
@@ -15,12 +29,12 @@ function changeStyle(){
         $("#screen").css("background-color", "#B2E5F0");
         $("img").css("color", "#B2E5F0");
         $("body").css("background-image", "url(../images/background.png");
-        style=0;
+        style = 0;
     }else{
         $(".btn").css("background-color", "");
         $("#screen").css("background-color", "");
         $("img").css("color", "");
         $("body").css("background-image", "");
-        style=1;
+        style = 1;
     }
 }
