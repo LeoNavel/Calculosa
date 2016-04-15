1d28cb02412b691b7319642af259ceed6c92d779
Filip Klembara <leo@navel.pro>
2016-04-15 22:44:03 +0200

    reformat code and add comments

--- app/js/Mathlib/Mathlib.js
+++ app/js/Mathlib/Mathlib.js
@@ -51,17 +51,19 @@ return a*b;
  *	@return logarithm of a
  */
 exports.logarithm = function(x){
-    var n=1, eps= 1, log1, log;
-    while (eps>=0.00000001){
-        n=n*2;
-        log = continuedFraction(x,n);
-        log1 = continuedFraction(x,n-1);
-        eps = log1-log;
-        if (eps<0) {
+    var n = 1, eps = 1, log1, log;
+    if(x==0)
+        return -Infinity;
+    while(eps>=0.00000001){
+        n = n * 2;
+        log = continuedFraction(x, n);
+        log1 = continuedFraction(x, n - 1);
+        eps = log1 - log;
+        if(eps<0){
             eps = eps * -1;
         }
     }
-    return continuedFraction(x,n);
+    return continuedFraction(x, n);
 };
 
 /** @brief Continued Fraction
@@ -69,22 +71,22 @@ exports.logarithm = function(x){
  *  Calculates logarithm for n iterations from continued fractions
  *
  *  @param [in] n number
- *	@param [in] a number
- *	@return logarithm of a
+ *    @param [in] a number
+ *    @return logarithm of a
  */
-continuedFraction = function(x,n){
+continuedFraction = function(x, n){
     if(x<0 || n<=0)
         return NaN;
     if(x>0){
-        x = (x-1)/(x+1);    //upravenie
+        x = (x - 1) / (x + 1);    //upravenie
         n--;        //dekrementovanie kvoli tomu ze prva iteracia je v return
-        var menovatel = (n==0)?1:2*n;    //nastavenie najspodnejsieho menovatela
+        var menovatel = (n==0) ? 1 : 2 * n;    //nastavenie najspodnejsieho menovatela
         while(n>0){
-            menovatel = 2.0*n-1-(x*x*n*n/menovatel);  //nastavenie noveho menovatela
+            menovatel = 2.0 * n - 1 - (x * x * n * n / menovatel);  //nastavenie noveho menovatela
             n--;        //dekrementovanie /(n)
         }
 
-        return 2*x/menovatel;       //vrati vysledok pre dany pocet operacii
+        return 2 * x / menovatel;       //vrati vysledok pre dany pocet operacii
     }
 };
 
@@ -97,40 +99,40 @@ continuedFraction = function(x,n){
  *	@return b-th power of a
  */
 exports.power = function(a,b){
-    var i= 1, c= 1, sign= b;
+    var i = 1, c = 1, sign = b;
     var nasobky = [], mocniny = [];
     if (b<0){
         b = b*-1;
     }
-    if (a == 0){
+    if(a==0){
         return a;
     }
 
-    if (a>0 && a<1 || a<0 && a>-1){
-        while(i<=b) {
+    if(a>0 && a<1 || a<0 && a> -1){
+        while(i<=b){
             c = c * a;
             i++;
         }
         if(sign<0){
-            c=1/c;
+            c = 1 / c;
         }
         return c;
     }
 
-    nasobky[0]=1;
-    mocniny[0]=a;
+    nasobky[0] = 1;
+    mocniny[0] = a;
 
-    while(nasobky[i-1]<b) {
-        nasobky[i] = nasobky[i-1]*a;
-        mocniny[i] = mocniny[i-1]*mocniny[i-1];
+    while(nasobky[i - 1]<b){
+        nasobky[i] = nasobky[i - 1] * a;
+        mocniny[i] = mocniny[i - 1] * mocniny[i - 1];
         i++;
     }
 
     i--;
-    while (i>=0) {
-        if (b-nasobky[i]>=0){
-            b=b-nasobky[i];
-            c*=mocniny[i];
+    while(i>=0){
+        if(b - nasobky[i]>=0){
+            b = b - nasobky[i];
+            c *= mocniny[i];
         }
         i--;
     }
@@ -167,18 +169,18 @@ exports.division = function(a,b){
  *	@return factorial of a
  */
 
-exports.factorial = (function() {
+exports.factorial = (function(){
     var cache = {},
-        fn = function(n) {
-            if (n < 0){
+        fn = function(n){
+            if(n<0){
                 return NaN;
             }
-            if (n === 0) {
+            if(n===0){
                 return 1;
-            } else if (cache[n]) {
+            }else if(cache[n]){
                 return cache[n];
             }
-            return cache[n] = n * fn(n -1);
+            return cache[n] = n * fn(n - 1);
         };
     return fn;
 }());
--- app/js/script.js
+++ app/js/script.js
@@ -18,13 +18,14 @@ var olejvan = {state: 1, vars: {a: "", b: "", operation: ""}};
 var screen = {operation: "", output: "", operationStr: "", outputStr: "", newNumber: true};
 ///everything with screen
 
-/** @brief handle click events
+/**
+ * @brief handle click events
  *
  * @return There is no return from this hell
  */
 $(function(){
     $(".btn").click(function(){
-        if($(this).attr("id") == "btnHelp"){
+        if($(this).attr("id")=="btnHelp"){
             Calculosa.help();
             return;
         }
@@ -50,17 +51,17 @@ $(function(){
             }
         }else{
             ids = $(this).attr("id");
-                if(ids=="btnPoint")
-                    floatingPoint();
-                if(ids=="btnSign")
-                    changeSign();
-                if(ids=="btnDel")
-                    deleteLastNumber();
-                if(ids=="btnCA")
-                    clearAll();
+            if(ids=="btnPoint")
+                floatingPoint();
+            if(ids=="btnSign")
+                changeSign();
+            if(ids=="btnDel")
+                deleteLastNumber();
+            if(ids=="btnCA")
+                clearAll();
             if(ids=="btnSolve")
                 solve(true);
-            }
+        }
         setScreenData();
         if(Calculosa.somethingToSay)
             Calculosa.say();
@@ -71,22 +72,27 @@ $(function(){
     });
 });
 
-
-function doBinOperand(ids){
-    ids = $(this).attr("id");
+/**
+ * @brief solve for binary operand
+ *
+ * checking state and solve
+ *
+ */
+function doBinOperand(){
+    var ids = $(this).attr("id");   //get id
     if(ids.length>0){
         switch(olejvan.state){
             case 1:
             case 3:
                 if(screen.outputStr=="")
                     screen.outputStr = "0";
-                screen.operationStr = $(this).text();
+                screen.operationStr = $(this).text();   //set operation
                 changeStateTo(2);
                 break;
             case 2:
                 if(!screen.newNumber){
-                    solve(true);
-                    screen.operationStr = $(this).text();
+                    solve(true);    //solve for binary operand
+                    screen.operationStr = $(this).text(); //set operation
                 }
                 changeStateTo(2);
                 break;
@@ -94,24 +100,30 @@ function doBinOperand(ids){
     }
 }
 
-function doUnOperand(ids){
-    ids = $(this).attr("id");
+/**
+ * @brief solve for unary operands
+ *
+ * checking state and solve
+ *
+ */
+function doUnOperand(){
+    var ids = $(this).attr("id");
     if(ids.length>0){
         switch(olejvan.state){
             case 1:
             case 3:
                 if(screen.outputStr=="")
                     screen.outputStr = "0";
-                screen.operationStr = $(this).text();
+                screen.operationStr = $(this).text();   //set operation
                 solve(false);
                 break;
             case 2:
                 if(!screen.newNumber){
-                    solve(true);
-                    screen.operationStr = $(this).text();
-                    solve(false);
+                    solve(true);    //solve previous binary operation
+                    screen.operationStr = $(this).text(); //set operation
+                    solve(false);   //solve unary operation
                 }else{
-                    screen.operationStr = $(this).text();
+                    screen.operationStr = $(this).text();   //set operation
                     solve(false);
                 }
                 break;
@@ -119,7 +131,9 @@ function doUnOperand(ids){
     }
 }
 
-/** @brief solve for binary operands
+/** @brief solve binary operand
+ *
+ * detect which function should by called and call it
  *
  */
 function solveBin(){
@@ -127,8 +141,8 @@ function solveBin(){
     var a = olejvan.vars.a,
         b = olejvan.vars.b,
         op = olejvan.vars.operation;
-    a = +a;
-    b = +b;
+    a = +a; //str to number
+    b = +b; //str to number
     var func;
     switch(op){
         case "-":
@@ -161,6 +175,13 @@ function solveBin(){
     }
 }
 
+/**
+ *
+ * @brief solve unary
+ *
+ * decide which function should be used and call it
+ *
+ */
 function solveUn(){
     olejvan.vars.a = screen.outputStr;
     var a = olejvan.vars.a,
@@ -189,6 +210,13 @@ function solveUn(){
     }
 }
 
+/**
+ *
+ * @brief reload screen data
+ *
+ * reloads data from screen
+ *
+ */
 function reloadScreenData(){
     screen.operation = $("#operation");
     screen.operationStr = screen.operation.text();
@@ -196,11 +224,27 @@ function reloadScreenData(){
     screen.outputStr = screen.output.text();
 }
 
+/**
+ *
+ * @brief sets screen data
+ *
+ * sets data on screen
+ *
+ */
 function setScreenData(){
     screen.operation.text(screen.operationStr);
     screen.output.text(screen.outputStr);
 }
 
+/**
+ *
+ * @brief change state
+ *
+ * change current state
+ *
+ * @param state next state
+ *
+ */
 function changeStateTo(state){
     switch(state){
         case 1:
@@ -228,6 +272,15 @@ function changeStateTo(state){
     }
 }
 
+/**
+ *
+ * @brief solve
+ *
+ * try to solve everything
+ *
+ * @param bin if true, solve for binary; if not, solve for unary
+ *
+ */
 function solve(bin){
     var e = false;
     if(bin){
@@ -256,6 +309,13 @@ function solve(bin){
 
 }
 
+/**
+ *
+ * @brief clear
+ *
+ * clear everything and set current state to 1
+ *
+ */
 function clearAll(){
     Calculosa.setSentence("Clear!");
     olejvan.state = 1;
@@ -267,6 +327,9 @@ function clearAll(){
     setScreenData();
 }
 
+/**
+ * @brief delete last number from screen
+ */
 function deleteLastNumber(){
     var str = screen.outputStr;
     if(str.length>0){
@@ -277,6 +340,9 @@ function deleteLastNumber(){
     }
 }
 
+/**
+ * @brief change sign
+ */
 function changeSign(){
     var str = screen.outputStr;
     if(str.length>0){
@@ -290,8 +356,16 @@ function changeSign(){
     }
 }
 
+/**
+ * @brief add number to screen
+ *
+ * add number if the number is "new" then clear screen and insted of add make the number first
+ *
+ * @param element element of the clicked button
+ */
 function addNumber(element){
-
+    /// maximal length of string on screen
+    var maxLen = 30;
     if(olejvan.state==3){
         changeStateTo(1);
     }
@@ -300,7 +374,7 @@ function addNumber(element){
         str = "";
         screen.newNumber = false;
     }
-    if(str.length<30){
+    if(str.length<maxLen){
         str += element.text();
         screen.outputStr = str;
     }else{
@@ -308,6 +382,12 @@ function addNumber(element){
     }
 }
 
+/**
+ * @brief add flating point
+ *
+ * if last char on the screen is point then removes it
+ * if on the screen is point and it is not last char do nothing
+ */
 function floatingPoint(){
     var str = screen.outputStr;
     if(screen.newNumber){
@@ -317,7 +397,7 @@ function floatingPoint(){
     if(str.length>0){
         if(str.slice(-1)==".")
             str = str.slice(0, -1);
-        else if(str.indexOf(".")<0)
+        else if(str.indexOf(".")<0) //check if point exists
             str += ".";
         else{
             Calculosa.setErr("you can have just one point");  //todo calculosa error
@@ -325,7 +405,7 @@ function floatingPoint(){
         }
         screen.outputStr = str;
     }else
-        screen.outputStr = "0.";
+        screen.outputStr = "0.";    //if screen#output is empty add 0. to it
 }
 
 function changeStyle(){
@@ -343,3 +423,5 @@ function changeStyle(){
         style = 1;
     }
 }
+
+/*** End of file script.js ***/
